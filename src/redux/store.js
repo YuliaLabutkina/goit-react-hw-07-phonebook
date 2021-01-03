e9b64cba93685ef6b35c5e4073cb003ed2377bb4
Labutkina Yulia <labutkinayuliaa@gmail.com>
2021-01-03 23:44:07 +0200

    added hw-07-phonebook

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,32 +1,3 @@
-// import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-// import {
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-// import phonebookReducer from './phonebook/phonebook-reducer';
-
-// const middleware = [
-//   ...getDefaultMiddleware({
-//     serializableCheck: {
-//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//     },
-//   }),
-// ];
-
-// const store = configureStore({
-//   reducer: {
-//     contacts: phonebookReducer,
-//   },
-//   devTools: process.env.NODE_ENV === 'development',
-//   middleware,
-// });
-
-// export { store };
-
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
